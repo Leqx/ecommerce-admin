75caa94e14be48c6f3c241b0f0c0ede52f241000
Wahomethegeek <gatwirikelvin@outlook.com>
2024-04-24 21:01:53 +0300

    :package:

---
+++ main/main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const serve = require("electron-serve");
+const path = require("path");
+
+const appServe = app.isPackaged ? serve({
+  directory: path.join(__dirname, "../out")
+}) : null;
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      webSecurity: false
+    }
+  });
+
+  if (app.isPackaged) {
+    appServe(win).then(() => {
+      win.loadURL("app://-");
+    });
+  } else {
+    ipcMain.on("authtoken", (event, arg) => {
+      // Handle the auth token data sent from the auth window
+      console.log("Received auth token:", arg);
+
+      // Close the authentication window
+      win.webContents.send("close-auth");
+    });
+
+    win.loadURL("http://localhost:3000");
+    win.webContents.openDevTools();
+    win.webContents.on("did-fail-load", (e, code, desc) => {
+      win.webContents.reloadIgnoringCache();
+    });
+  }
+};
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
